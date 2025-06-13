6b70858dcc56620bd8a0b6613034935483b48050
Yui-220053 <te220053@gmail.com>
2025-06-13 14:10:29 +0900

    kadai9

--- src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookController.java
@@ -1,14 +1,13 @@
-package jp.te4a.spring.boot.myapp8.mybootapp8;
+package jp.te4a.spring.boot.myapp9.mybootapp9;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("books")
@@ -28,26 +27,26 @@ public class BookController {
     }
 
     @PostMapping(path = "create")
-    String create(BookForm form, Model mode) {
-        bookService.create(form);
+    String create(BookForm form, Model model) {
+        bookService.save(form);
         return "redirect:/books";
     }
 
-    @PostMapping(path = "edit", params = "form")
-    String editForm(@RequestParam Integer id, BookForm form) {
+    @GetMapping("edit")
+    public String editForm(@RequestParam Integer id, Model model) {
         BookForm bookForm = bookService.findOne(id);
-        BeanUtils.copyProperties(bookForm, form);
+        model.addAttribute("bookForm", bookForm);
         return "books/edit";
     }
 
-    @PostMapping(path = "edit")
-    String edit(@RequestParam Integer id, BookForm form) {
+    @PostMapping("edit")
+    public String edit(BookForm form) {
         bookService.update(form);
         return "redirect:/books";
     }
 
-    @PostMapping(path = "delete")
-    String delete(@RequestParam Integer id) {
+    @PostMapping("delete")
+    public String delete(@RequestParam Integer id) {
         bookService.delete(id);
         return "redirect:/books";
     }
--- src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookService.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookService.java
@@ -1,34 +1,38 @@
-package jp.te4a.spring.boot.myapp8.mybootapp8;
+package jp.te4a.spring.boot.myapp9.mybootapp9;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+//import java.util.ArrayList;
+//import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+//import jp.te4a.spring.boot.myapp9.mybootapp9.BookForm;
 
 @Service
 public class BookService {
     @Autowired
     BookRepository bookRepository;
 
-    public BookForm create(BookForm bookForm) {
-        bookForm.setId(bookRepository.getBookId());
+    public BookForm save(BookForm bookForm) {
         BookBean bookBean = new BookBean();
         BeanUtils.copyProperties(bookForm, bookBean);
-        bookRepository.create(bookBean);
+        bookRepository.save(bookBean);
         return bookForm;
     }
 
+    
+
     public BookForm update(BookForm bookForm) {
-        BookBean bookBean = new BookBean();
-        BeanUtils.copyProperties(bookForm, bookBean);
-        bookRepository.update(bookBean);
-        return bookForm;
+        return save(bookForm);
     }
 
     public void delete(Integer id) {
-        bookRepository.delete(id);
+        bookRepository.deleteById(id);
     }
 
     public List<BookForm> findAll() {
@@ -43,9 +47,11 @@ public class BookService {
     }
 
     public BookForm findOne(Integer id) {
-        BookBean bookBean = bookRepository.findOne(id);
+    Optional<BookBean> optionalBookBean = bookRepository.findById(id);
+    if (optionalBookBean.isPresent()) {
         BookForm bookForm = new BookForm();
-        BeanUtils.copyProperties(bookBean, bookForm);
+        BeanUtils.copyProperties(optionalBookBean.get(), bookForm);
         return bookForm;
     }
-}
+    return null; 
+}}
